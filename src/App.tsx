b3a1687543d24a45dd24cc9b49fecad7353e2d9b
toan <toan.nguyen@classmethod.jp>
2021-01-12 10:01:22 +0700

    add image and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 100,
+    padding: 20
+  },
+
+  image: {
+    width: 50, 
+    height: 100, 
+    backgroundColor: "blue", 
+    flex: 1
+  },
+  description: {
+    backgroundColor: "red", 
+  },
+  titletext: {
+    fontSize: 20, fontWeight: "bold", backgroundColor: "red", 
+  },
+  category: {
+    backgroundColor: "red", 
+  },
+});
 
 function App() {
   return (
-    <View>
-      <Text>Hello React Native for Web!</Text>
+    <View
+      style={styles.container}
+    >
+      <Image
+        style={styles.image}
+        resizeMode="contain"
+        source={{       
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <View style={[styles.description, {flex:2}]}>
+        <Text style={[styles.titletext, {flex:2}]}>Hello World!</Text>
+        <Text style={[styles.category, {flex:10}]}>Hello World!</Text>
+      </View>
+      
     </View>
   );
-}
+};
 
 export default App;
