243b26f233449aa7d27db95dfa744abf25944ebf
Takagi Kensuke <janus.wel.3@gmail.com>
2021-01-13 14:00:44 +0900

    :art: refactor for readability

--- src/App.tsx
+++ src/App.tsx
@@ -7,58 +7,32 @@ import {
 } from "react-native";
 
 import Item from "./listItem";
-import {NewsItem} from "./data";
+import newsItems from "./data";
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    height: 100,
-    padding: 20
-  },
-
-  image: {
-    backgroundColor: "blue", 
-    flex: 1
-  },
-  description: {
-    backgroundColor: "red", 
-  },
-  titletext: {
-    fontSize: 20, fontWeight: "bold", backgroundColor: "red", 
-  },
-  category: {
-    backgroundColor: "red", 
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+    textAlign: 'center',
   },
 });
 
 function App() {
   return (
-    <View>
-    <View style={styles.container}>
-      <Image
-        style={styles.image}
-        resizeMode="contain"
-        source={{       
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-      <View style={[styles.description, {flex:2}]}>
-        <Text style={[styles.titletext, {flex:2}]}>Hello World!</Text>
-        <Text style={[styles.category, {flex:1}]}>Hello World!</Text>
-      </View>
-    
-      
-    </View>
     <SafeAreaView>
-    <FlatList
-        data={NewsItem}
-        renderItem={({item})=>(
-          <Item id={item.id} uri={item.uri} title={item.title} category={item.category} />
+      <Text style={styles.title}>New Gears</Text>
+      <FlatList
+        data={newsItems}
+        renderItem={({item}) => (
+          <Item
+            id={item.id}
+            uri={item.uri}
+            title={item.title}
+            category={item.category}
+          />
         )}
       />
-        </SafeAreaView>
-
-    </View>
+    </SafeAreaView>
   );
 };
 
