b2900241e7620da764d460fff73b29730177063c
toan <toan.nguyen@classmethod.jp>
2021-01-18 10:26:35 +0700

    setup redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,6 +9,32 @@ import {
 import Header from './Header'
 import Routes from './Routes'
 
+import { createStore } from 'redux'
+
+interface Action{
+  type:string;
+}
+
+function counterReducer(state = { value: 0 }, action:Action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 }
+    case 'counter/decremented':
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
+let store = createStore(counterReducer);
+debugger
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({ type: 'counter/incremented' });
+
+store.dispatch({ type: 'counter/incremented' });
+
+store.dispatch({ type: 'counter/decremented' });
+
 function App() {
   return (
     <SafeAreaView>
