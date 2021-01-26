b935dbf3ef967f2e8cbaa9c7125a54670992f3f8
toan <toan.nguyen@classmethod.jp>
2021-01-26 13:59:10 +0700

    add test cases

--- src/modules/counter.test.ts
+++ src/modules/counter.test.ts
@@ -1,5 +1,35 @@
-import {createInitialState} from "./counter";
+import * as Counter from "./counter";
 
-test('return object {value:0}', () => {
-    expect(createInitialState()).toEqual({value:0});
-  });
+describe("counter",()=>{
+  describe("createInitialState",()=>{
+    test('return object {value:0}', () => {
+      expect(Counter.createInitialState()).toEqual({value:0});
+    });
+  })
+  describe("increment",()=>{
+    test('return type: COUNTER_INCREMENT', () => {
+      expect(Counter.increment()).toEqual({type: Counter.COUNTER_INCREMENT});
+    });
+  })
+  describe("decrement",()=>{
+    test('return type: COUNTER_DECREMENT', () => {
+      expect(Counter.decrement()).toEqual({type: Counter.COUNTER_DECREMENT});
+    });
+  })
+  describe("reducer",()=>{
+    test('return object {value:1} when passed increment action', () => {
+      expect(Counter.default(undefined,{
+        type: Counter.COUNTER_INCREMENT
+      })).toEqual({value: 1});
+    });
+    test('return object {value:-1} when passed decrement action', () => {
+      expect(Counter.default(undefined,{
+        type: Counter.COUNTER_DECREMENT
+      })).toEqual({value: -1});
+    });
+    test('return object {value:0} when passed unknown action', () => {
+      expect(Counter.default(undefined,{type:"foo"} as Counter.Action)).toEqual({value: 0});
+    });
+  })
+})
+
