5214fb5882f23182853449ef601e751da50e6339
toan <toan.nguyen@classmethod.jp>
2021-01-28 09:59:54 +0700

    add liff login

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import liff from '@line/liff';
+
+liff.init({ liffId: '1655616771-0P26KVbm' });
+async function login() {
+    if (!liff.isLoggedIn()) {
+        liff.login();
+    }
+    const profile = await liff.getProfile();
+    console.log(profile);
+}
+setTimeout(login, 1000);
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
