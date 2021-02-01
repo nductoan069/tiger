5b72a2b54ba1919a9b062dd90da97a026cd0e76c
Takagi Kensuke <janus.wel.3@gmail.com>
2021-02-01 11:54:58 +0900

    :art: refactor login codes

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import liff from '@line/liff';
 
-liff.init({ liffId: '1655616771-0P26KVbm' });
+async function init() {
+  try {
+    await liff.init({ liffId: '1655616771-0P26KVbm' })
+    await login();
+  }
+  catch(error) {
+    console.error(`failed: ${error.message}`)
+  }
+}
+
 async function login() {
     if (!liff.isLoggedIn()) {
         liff.login();
-    } 
+    }
+
     const profile = await liff.getProfile();
     console.log(profile);
+
+    ReactDOM.render(<App />, document.getElementById("root"));
 }
-setTimeout(login, 1000);
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+init()
