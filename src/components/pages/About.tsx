9730d71f03c3e6521a7a862f2f85f692d5446df5
toan <toan.nguyen@classmethod.jp>
2021-02-02 10:09:32 +0700

    add sendMessages function

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -2,11 +2,25 @@ import React, { FC, useState  } from "react";
 import {
     Text, TouchableOpacity, TextInput
   } from "react-native";
+import liff from '@line/liff';
 
 interface Props{
   name:string;
   setName:(name:string)=> void;
 }
+async function sendMessages() {
+  try {
+    await liff.sendMessages([
+      {
+        type: 'text',
+        text: 'Hello, World!'
+      }
+    ])
+    console.log('message sent');
+  } catch(err) {
+      console.log('error', err);
+    }
+};
 
   const About: FC<Props> = (props) => {
     const [name, setName] = useState("");
@@ -24,6 +38,11 @@ interface Props{
         onPress={()=>{props.setName(name)}} 
         >
         <Text>Press Here</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+        onPress={sendMessages} 
+        >
+        <Text>Send Message</Text>
         </TouchableOpacity>
       </>
     );
